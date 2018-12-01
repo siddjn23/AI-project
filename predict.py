424cf7a4b20acbf5dfd6d74798d87f689e005ec7
Siddharth jain <35165998+siddjn23@users.noreply.github.com>
2018-12-02 01:44:49 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-# Imports here
+# Imports 
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,45 +13,35 @@ import torch.nn.functional as F
 import copy
 from PIL import Image
 
-def preprocess_image_(image):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+def process_image(image):
+    ''' Scales,croProbability,and normalizes a PIL image for a PyTorch model,
         returns an Numpy array
     '''
     
     # TODO: Process a PIL image for use in a PyTorch model
-    Size_image=256
-    image.thumbnail(Size_image, Image.ANTIALIAS)
+    Image_Length=256
+    image.thumbnail(Image_Length,Image.ANTIALIAS)
     image=image.crop((128 - 112,128-112,128+112,128 + 112))
-    Image = np.array(image)
-    Me_Image = Image/255
-        
-    X_1=Me_Image[:,:,0]
-    X_2=Me_Image[:,:,1]
-    X_3=Me_Image[:,:,2]
+    Image_Variables=np.array(image)
+    Image_Variables=Image_Variables/255
+            
+    A1=(Image_Variables[:,:,0]-0.485)/(0.229) 
+    A2=(Image_Variables[:,:,1]-0.456)/(0.224)
+    A3=(Image_Variables[:,:,2]-0.406)/(0.225)
     
-    X1=(X_1-0.485)/(0.229) 
-    X2=(X_2-0.456)/(0.224)
-    X3=(X_3-0.406)/(0.225)
+    Image_Variables[:,:,0]=A1
+    Image_Variables[:,:,1]=A2
+    Image_Variables[:,:,2]=A3
     
-    Me_Image[:,:,0]=X1
-    Me_Image[:,:,1]=X2
-    Me_Image[:,:,2]=X3
-    
-    Me_Image = np.transpose(Me_Image, (2,0,1))
-    return Me_Image
+    Image_Variables=np.transpose(Image_Variables,(2,0,1))
+    return Image_Variables
 
 
 # TODO: load the checkpoint
 def load_checkpoint(filepath):
-
-    checkpoint = torch.load(args.saved_model)
-    if checkpoint['arch'] == 'vgg':
-        model = models.vgg16()  
-        initial_input= model.classifier[0].in_features
-    elif checkpoint['arch'] == 'densenet':
-        model = models.densenet121()
-        initial_input= model.classifier.in_features
-        
+    checkpoint = torch.load(filepath)
+    model = models.vgg16()
+    initial_input= model.classifier[0].in_features
     classifier = nn.Sequential(OrderedDict([
                               ('fc1', nn.Linear(initial_input, 512)),
                               ('relu', nn.ReLU()),
@@ -69,29 +59,29 @@ def load_checkpoint(filepath):
             print ("Using GPU")
         else:
             print("Using CPU")
-    loaded_model, class_2_idx = load_checkpoint('my_checkpoint.pth')
-    idx_2_class = { v : k for k,v in class_2_idx.items()}
-    return model, class_2_idx, idx_2_class
+    loaded_model, class_to_idx = load_checkpoint('my_checkpoint.pth')
+	idx_to_class = { v : k for k,v in class_to_idx.items()}
+    return model, class_to_idx, idx_to_class
 
 def predict(args, image_path, model, class_to_idx, idx_to_class, cat_to_name, topk=5):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''
     # TODO: Implement the code to predict the class from an image file
-    Me_Image = torch.FloatTensor([process_image(Image.open(image_path))])
+    My_Image = torch.FloatTensor([process_image(Image.open(image_path))])
     if args.gpu and torch.cuda.is_available():
         model = model.cuda()
     model.eval()
     if args.gpu and torch.cuda.is_available():
-        Result = model.forward(Variable(Me_Image.cuda()))
+        Result = model.forward(Variable(My_Image.cuda()))
     else:
-        Result = model.forward(Variable(Me_Image))
-    P_S = torch.exp(Result).data.numpy()[0]
+        Result = model.forward(Variable(My_Image))
+    PS = torch.exp(Result).data.numpy()[0]
     Top_Index = np.argsort(ps)[-topk:][::-1] 
     Top_Class = [idx_to_class[x] for x in Top_Index]
-    Probability_dist = P_S[Top_Index]
+    Probability = PS[Top_Index]
 
-    print("Probability is --> {} \n and class is--> {}".format(Probability_dist, Top_Class))
-    return Probability_dist, Top_Class
+    print("Probability is : {} \n Top class is : {}".format(Probability, Top_Class))
+    return Probability, Top_Class
 
 if __name__ == "__main__":
 
@@ -106,12 +96,12 @@ if __name__ == "__main__":
 
     import json
     with open(args.mapper_json, 'r') as f:
-        cat = json.load(f)
+        cat_to_name = json.load(f)
 
-    model, class_2_idx, idx_2_class = load_checkpoint(args)
-    T_Probability, T_class = predict(args, args.image_path, model, class_to_idx, idx_to_class, cat_to_name, topk=args.topk)
+    model, class_to_idx, idx_to_class = load_checkpoint(args)
+    Top_Probability, Top_class = predict(args, args.image_path, model, class_to_idx, idx_to_class, cat_to_name, topk=args.topk)
                                               
-    print('Predicted Classes is ---> ', T_Class)
-    print ('Class Names:---> ')
-    [print(cat[x]) for x in T_Class]
-    print('Predicted Probability-----> ', T_Probability)
+    print('Predicted Classes: ', Top_Class)
+    print ('Class Names: ')
+    [print(cat_to_name[x]) for x in Top_Class]
+    print('Predicted Probability: ', Top_Probability)
